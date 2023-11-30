a3a4c1e7354f35f559cc94ef4f239f86e5c94cbf
myway-it <57533048+myway-it@users.noreply.github.com>
2023-11-30 14:59:20 +0300

    add models (#10)
    * add models
    
    * rm pycache

---
+++ src/models/enums.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+
+class District(str, Enum):
+    """Saint Petersburg districts."""
+
+    admiralteysky = "admiralteysky"
+    vasileostrovsky = "vasileostrovsky"
+    vyborg = "vyborg"
+    kalininsky = "kalininsky"
+    kirovsky = "kirovsky"
+    kolpinsky = "kolpinsky"
+    krasnogvardeisky = "krasnogvardeisky"
+    krasnoselsky = "krasnoselsky"
+    kronstadt = "kronstadt"
+    kurortny = "kurortny"
+    moscow = "moscow"
+    nevsky = "nevsky"
+    petrogradsky = "petrogradsky"
+    petrodvortsovy = "petrodvortsovy"
+    primorky = "primorky"
+    pushkinsky = "pushkinsky"
+    frunzensky = "frunzensky"
+    central = "central"
+
+
+class Cuisine(str, Enum):
+    """Supported cuisines."""
+
+    russia = "russia"
+    china = "china"
+    japan = "japan"
+    georgia = "georgia"
+    italy = "italy"
+    korea = "korea"
+    mexico = "mexico"
+    uzbekistan = "uzbekistan"
+
+
+class CafeType(str, Enum):
+    """Supported cafe types."""
+
+    restaurant = "restaurant"
+    cafe = "cafe"
+    canteen = "canteen"
