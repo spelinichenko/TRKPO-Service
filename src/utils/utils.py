ee5a3f5444d4b859bb4093858ba70ce1e7147658
myway-IT <kossamoilenko@gmail.com>
2023-12-04 21:09:51 +0300

    add lifespan func

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,6 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
 from src.models.models import MainResponse
 
 mock_response = MainResponse(
@@ -9,3 +12,9 @@ mock_response = MainResponse(
     working_hours="test",
     delivery=True,
 )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> None:
+    # PUT BD SCRIPT HERE
+    yield
