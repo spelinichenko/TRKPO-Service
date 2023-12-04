e57b3ef22383546467ac2c5065a672589d6c0dc0
myway-IT <kossamoilenko@gmail.com>
2023-12-05 00:36:59 +0300

    add parser, pandas

---
+++ src/utils/parser.py
@@ -0,0 +1,48 @@
+# DEPRECATED!!!
+
+from bs4 import BeautifulSoup
+import requests
+import time
+import random
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.2; WOW32; rv:20.0) Gecko/20100101 Firefox/20.0"}
+result = set()
+
+main_page = requests.get(
+    "https://spb.cian.ru/snyat-pomeshenie-pod-obshepit/",
+    headers=headers,
+)
+time.sleep(random.randint(2, 10))
+
+for i in range(1, 14):
+    print(f"-------------{i}------------")
+
+    page = requests.get(
+        f"https://spb.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=offices&office_type%5B0%5D=4&p={i}&region=2",
+        headers=headers,
+    )
+    time.sleep(random.randint(2, 10))
+
+    soup = BeautifulSoup(page.text, "lxml")
+    all_publications = soup.find_all("div", {"class": "_8c559fa4c7--card-wrapper--hR8ns"})
+
+    for publication in all_publications:
+        """space_h2 = publication.find("h2").contents[0]
+        if "м²" not in space_h2:
+            continue
+        space = space_h2.split(" ")[-2]"""
+
+        card_price_block = publication.find("div", {"data-name": "CardPriceBlock"})
+        cost = card_price_block.find("span", {"style": "letter-spacing:-0.5px"})
+        cost = cost.contents[0].replace("\xa0", "").replace("₽/мес.", "")
+        pure_cost = int("".join(filter(str.isdigit, cost)))
+
+        car_address_block = publication.find("div", {"data-name": "CardAddressBlock"})
+        address_elements = car_address_block.find_all("a", {"data-name": "InteractiveTextFactory"})
+        street = address_elements[-2].contents[0][:-2]
+        full_address = "".join([a_e.contents[0] for a_e in address_elements])
+
+        print(pure_cost, full_address, street)
+        result.add(full_address)
+
+print(len(result))
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,29 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+import pandas as pd
 
 from src.models.models import MainResponse
+from src.models.enums import District
+
+district_to_url = {
+    District.admiralteysky: "admiralteyskiy-04150",
+    District.central: "centralnyy-04133",
+    District.frunzensky: "frunzenskiy-04134",
+    District.kalininsky: "kalininskiy-04147",
+    District.kirovsky: "kirovskiy-04146",
+    District.kolpinsky: "kolpinskiy-04145",
+    District.krasnogvardeisky: "krasnogvardeyskiy-04144",
+    District.krasnoselsky: "krasnoselskiy-04143",
+    District.kurortny: "kurortnyy-04141",
+    District.moscow: "moskovskiy-04140",
+    District.nevsky: "nevskiy-04139",
+    District.petrodvortsovy: "petrodvorcovyy-04137",
+    District.petrogradsky: "petrogradskiy-04138",
+    District.primorsky: "primorskiy-04136",
+    District.pushkinsky: "pushkinskiy-04135",
+    District.vasileostrovsky: "vasileostrovskiy-04149",
+    District.vyborg: "vyborgskiy-04148",
+}
 
 mock_response = MainResponse(
     street="test",
@@ -18,3 +40,9 @@ mock_response = MainResponse(
 async def lifespan(app: FastAPI) -> None:
     # PUT BD SCRIPT HERE
     yield
+
+
+def get_dataframe_by_district(district: District) -> pd.DataFrame:
+    return pd.read_excel(
+        f"https://spb.cian.ru/snyat-pomeshenie-pod-obshepit-sankt-peterburg-{district_to_url[district]}"
+    )
