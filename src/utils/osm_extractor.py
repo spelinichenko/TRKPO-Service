65ce28836dc324a8484aad13e30cb2ce8e97f1cd
Nyakutka <capov02@mail.ru>
2023-12-05 05:08:36 +0300

    fix osm extractor

--- src/utils/osm_extractor.py
+++ src/utils/osm_extractor.py
@@ -2,13 +2,14 @@ import osmnx as ox
 import geopandas as gpd
 import geopandas as gpd
 from src.models.enums import District
+import random
 
 def get_spb_boundaries():
     spb = ox.geocode_to_gdf("R337422", by_osmid=True)
     return spb.geometry.iloc[0]
 
 
-def get_spb_districts_from_osm(spb_boundaries, district: District) -> gpd.GeoDataFrame:
+def get_spb_district_from_osm(spb_boundaries, district: District) -> gpd.GeoDataFrame:
     districts_to_names = {
         District.primorsky:"Приморский район", 
         District.moscow:"Московский район",
@@ -28,21 +29,21 @@ def get_spb_districts_from_osm(spb_boundaries, district: District) -> gpd.GeoDat
         District.krasnogvardeisky:"Красногвардейский район",
         District.vyborg:"Выборгский район"
     }
-    spb_districts = ox.features_from_polygon(spb_boundaries, tags={"name": districts_to_names[district]})
-    spb_districts = spb_districts.dropna(subset=['addr:region'])
-    spb_districts = spb_districts[spb_districts['addr:region'] != 'Ленинградская область']
-    spb_districts = spb_districts.reset_index()[['geometry', "name"]]
-    return spb_districts
+    spb_district = ox.features_from_polygon(spb_boundaries, tags={"name": districts_to_names[district]})
+    spb_district = spb_district.dropna(subset=['addr:region'])
+    spb_district = spb_district[spb_district['addr:region'] != 'Ленинградская область']
+    spb_district = spb_district.reset_index()[['geometry', "name"]]
+    return spb_district
 
-def get_spb_food_places_from_osm(spb_boundaries) -> gpd.GeoDataFrame:
+def get_spb_food_places_by_district(spb_district) -> gpd.GeoDataFrame:
     tags = {
         "amenity": ["cafe", "fast_food", "food_court", "restaurant"]
     }
-    data = ox.features_from_polygon(spb_boundaries, tags=tags).reset_index()
+    data = ox.features_from_polygon(spb_district.geometry.iloc[0], tags=tags).reset_index()
     return data
 
-def spatial_join_food_places_and_districts(food_places: gpd.GeoDataFrame, districts: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    return gpd.sjoin(food_places, districts).rename(columns={'name_left': 'name', 'name_right': 'district'}).drop(columns=['index_right'])
+def spatial_join_food_places_and_district(food_places: gpd.GeoDataFrame, district_gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    return gpd.sjoin(food_places, district_gdf).rename(columns={'name_left': 'name', 'name_right': 'district'}).drop(columns=['index_right'])
 
 def preprocess_raw_data(food_places_and_districts):
     needed_columns = ["geometry", "opening_hours", "cuisine", "amenity", "delivery", "name", "district"]
@@ -71,8 +72,7 @@ def preprocess_raw_data(food_places_and_districts):
         }
     }
     food_places_and_districts.replace(map, inplace=True)
-    food_places_and_districts['delivery'].bfill(inplace=True)
-    food_places_and_districts['delivery'].ffill(inplace=True)
+    food_places_and_districts['delivery'] = food_places_and_districts.apply((lambda x: random.choice([True, False])), axis=1)
 
     time_pattern = r'(\d{2}:\d{2}-\d{2}:\d{2})'
     food_places_and_districts['opening_hours'] = food_places_and_districts['opening_hours'].str.extract(time_pattern)
@@ -103,3 +103,11 @@ def preprocess_raw_data(food_places_and_districts):
     food_places_and_districts['cuisine'].bfill(inplace=True)
     food_places_and_districts['cuisine'].ffill(inplace=True)
     return food_places_and_districts
+
+def get_preprocessed_data_by_district(district: District) -> gpd.GeoDataFrame:
+    spb_boundaries = get_spb_boundaries()
+    district_info = get_spb_district_from_osm(spb_boundaries, district)
+    food_places = get_spb_food_places_by_district(district_info)
+    food_places_with_district = spatial_join_food_places_and_district(food_places, district_info)
+    preprocessed_food_places_with_district = preprocess_raw_data(food_places_with_district)
+    return preprocessed_food_places_with_district
